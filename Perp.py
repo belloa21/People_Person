abbb529d8e540ac7bbfe9c2a8b1c69925b97057a
alexr <belloa@hartwick.edu>
2019-11-13 22:12:59 -0500

    Created the code

--- Perp.py
+++ Perp.py
@@ -1,1 +1,15 @@
+from Person_Thing import Person
 
+name = "Jack"
+
+height = "Five foot six"
+
+weight = int("225")
+
+hair_color = "Black"
+
+my_person = Person(name, height, weight, hair_color)
+my_person.description()
+my_person.change_hair_color("Green")
+my_person.weight_update(25)
+my_person.description()
---
+++ Person_Thing.py
@@ -0,0 +1,20 @@
+# Alex Bello
+# 11/13/2019
+# Defines The class People
+
+
+class Person:
+    def __init__(self, name, height, weight, hair_color):
+        self.name = name
+        self.height = height
+        self.weight = weight
+        self.hair_color = hair_color
+
+    def description(self):
+        print(f"""Hi my name is {self.name}, I'm {self.height} tall. I weight {self.weight}lbs, and I have {self.hair_color} hair.""")
+
+    def change_hair_color(self,new_color):
+        self.hair_color = new_color
+
+    def weight_update(self,more_pounds):
+        self.weight += more_pounds
